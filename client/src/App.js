05012d781b915f389efaa544755b4e15631e2374
rommomm <rommomm@i.ua>
2022-05-25 23:02:50 +0300

    added sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import SignIn from "./pages/Auth/SignIn";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <SignIn />
     </div>
   );
 }
