d30ef417e69c6c19f984e610d10e15a4dd64634b
rommomm <rommomm@i.ua>
2022-05-26 14:02:14 +0300

    added auth system

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,29 +1,45 @@
 const { Router } = require("express");
 const router = Router();
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
-router.post("/signup", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+router.post(
+  "/signup",
+  [
+    check("username", "Incorrect username").isString(),
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Incorrect password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Incorrect data",
+        });
+      }
+      const { username, email, password } = req.body;
 
-    const isUsed = await User.findOne({ email });
+      const isUsed = await User.findOne({ username });
 
-    if (isUsed) {
-      return res.status(300).json({ message: "ERROR" });
+      if (isUsed) {
+        return res.status(300).json({ message: "This user already exists" });
+      }
+
+      const user = new User({
+        username,
+        email,
+        password,
+      });
+
+      await user.save();
+
+      res.status(201).json({ message: "Successful registration" });
+    } catch (error) {
+      console.log("error", error);
     }
-
-    const user = new User({
-      username,
-      email,
-      password,
-    });
-
-    await user.save();
-
-    res.status(201).json({ message: "SUCCESS" });
-  } catch (error) {
-    console.log("error", error);
   }
-});
+);
 
 module.exports = router;
